1bf7ca5f6c162731a745636281755e836839e1ef
FathanBagas07 <145728133+FathanBagas07@users.noreply.github.com>
2026-09-01 14:18:28 +0700

    Feat: add style.css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,8 @@
-<!DOCTYPE html>
+<!DOCTYPE html lang="id">
 <html>
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>BPJS Barcode Aset</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
