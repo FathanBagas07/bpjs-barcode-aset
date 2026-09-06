a284cd61c1920776b29c625cf56e9ec05f9e0d46
FathanBagas07 <145728133+FathanBagas07@users.noreply.github.com>
2026-09-06 16:05:19 +0700

    Feat: adding a laravel seeder feature

---
+++ database/seeders/AssetSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AssetSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\Asset:create([
+            'nama_barang' => 'Laptop Asus TUF',
+            'kode_barcode' => 'AST001',
+            'lokasi' => 'Ruang IT',
+            'kondisi' => 'Baik'
+        ]);
+    }
+}
