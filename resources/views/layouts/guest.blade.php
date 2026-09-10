239062eb99f235e4fbca8aa201ebfbf4b5d24088
FathanBagas07 <145728133+FathanBagas07@users.noreply.github.com>
2026-09-10 16:32:24 +0700

    Fix: move the login and registration forms to the center of the screen on mobile

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -23,7 +23,7 @@
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+    <div class="min-h-screen flex flex-col justify-center items-center pt-6 sm:pt-0 bg-gray-100">
         <div>
             <a href="{{ route('dashboard') }}" class="navbar-brand">
                 <img src="{{ asset('images/bpjs-ketenagakerjaan-logo.png') }}" alt="BPJS Ketenagakerjaan"
